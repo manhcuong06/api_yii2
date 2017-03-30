f17621a70fc8dc1d00c98e74b47f9a9488e44a43
Cuong Nguyen <nguyencuong945@gmail.com>
2017-03-30 22:22:38 +0700

    fixed untracked files

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=your_database',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
--- config/web-local.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$config = [
-    'components' => [
-    ],
-];
-
-return $config;
