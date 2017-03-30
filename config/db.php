a14deb9d1d5e9e2b46512e2542af7201f10556f9
Cuong Nguyen <cuongnguyen@Ambitions-MacBook-Air.local>
2017-03-30 09:45:28 +0700

    master

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=book_store',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
