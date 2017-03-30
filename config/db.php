09f85110832c4cf3cc1e78a0d67f7b101350996e
Cuong Nguyen <nguyencuong945@gmail.com>
2017-03-30 22:10:19 +0700

    master

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=book_store',
+    'dsn' => 'mysql:host=localhost;dbname=your_database',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
